a1ec3d33738531d32c4b8e70ed64d6aa1be9631e
Anvesh Chidura <anveshchidura@Anveshs-MBP.lan>
2023-10-12 16:54:49 -0400

    added src folder

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-center py-11">
+      <h1>Netflix GPT</h1>
+    </div>
+  );
+}
+
+export default App;
